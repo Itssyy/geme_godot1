d8f4886c2ea80e414cad2c85b01781df9714504b
vlad <Kharsko1996@gmail.ru>
2024-05-17 01:08:52 +0300

    v2

--- Player1/player1.gd
+++ Player1/player1.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
+@export var health = 100
 const SPEED = 200.0
 const JUMP_VELOCITY = -320.0
 const GRAVITY = 19.0
 const JUMP_APEX_THRESHOLD = 10.0
 
 enum Player_state { JUMP, ATTACK, MOVE, DAMAGE, }
-
+signal health_changed(new_health)
 var current_state = Player_state.MOVE
 
 var is_jumping = false
@@ -18,86 +19,122 @@ var can_attack = true
 @onready var attack_area = $Area2D
 @onready var attack_shape_left = $Area2D/CollisionShape2DLeft
 @onready var attack_shape_right = $Area2D/CollisionShape2DRight
+@onready var health_label = $HealthLabel
+
+func _ready():
+	print("Player script ready")
+	$Area2D2.connect("body_entered", Callable(self, "_on_body_entered"))
+	connect("health_changed", Callable(self, "_on_health_changed"))
+	print("Connected health_changed signal")
+	_update_health_label()  # Обновляем метку при запуске
 
 func _physics_process(delta):
-		match current_state:
-				Player_state.MOVE:
-						move_state(delta)
-				Player_state.DAMAGE:
-						damage_state()
-				Player_state.JUMP:
-						jump_state(delta)
-				Player_state.ATTACK:
-						attack_state()
+	match current_state:
+		Player_state.MOVE:
+			move_state(delta)
+		Player_state.DAMAGE:
+			damage_state()
+		Player_state.JUMP:
+			jump_state(delta)
+		Player_state.ATTACK:
+			attack_state()
+
+func _on_body_entered(body):
+	print("Body entered area")
+	if body.has_method('_on_attack_area_entered'):
+		print("Body has method _on_attack_area_entered")
+		take_damage(body.damage)
+	else:
+		print("Body does not have method _on_attack_area_entered")
+
+func take_damage(damage: int):
+	print("Taking damage: ", damage)
+	health -= damage
+	emit_signal("health_changed", health)
+	print("Emitting health_changed signal with health: ", health)
+	if health <= 0:
+		die()
+
+func _on_health_changed(new_health):
+	print("Health changed: ", new_health)
+	_update_health_label()  # Обновляем метку при изменении здоровья
+
+func _update_health_label():
+	health_label.text = str(health)  # Обновляем текст метки
+
+func die():
+	print("Player died")
+	queue_free()
 
 func move_state(delta):
-		velocity.y += GRAVITY
+	velocity.y += GRAVITY
 
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-				velocity.y = JUMP_VELOCITY
-				is_jumping = true
-				is_falling = false
-				current_state = Player_state.JUMP
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		is_jumping = true
+		is_falling = false
+		current_state = Player_state.JUMP
 
-		var direction = Input.get_axis("ui_left", "ui_right")
-		if direction:
-				velocity.x = direction * SPEED
-				sprite.flip_h = direction < 0
-				anim_player.play("run")
-		else:
-				velocity.x = move_toward(velocity.x, 0, SPEED)
-				anim_player.play("idle")
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+		sprite.flip_h = direction < 0
+		anim_player.play("run")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		anim_player.play("idle")
 
-		if Input.is_action_just_pressed("attack") and can_attack:
-				current_state = Player_state.ATTACK
-				update_attack_area_position()
+	if Input.is_action_just_pressed("attack") and can_attack:
+		current_state = Player_state.ATTACK
+		update_attack_area_position()
 
-		move_and_slide()
+	move_and_slide()
 
 func damage_state():
-		velocity = Vector2.ZERO
-		anim_player.play("hurt")
+	velocity = Vector2.ZERO
+	anim_player.play("hurt")
 
 func jump_state(delta):
-		velocity.y += GRAVITY
+	velocity.y += GRAVITY
 
-		if is_jumping and velocity.y < 0:
-				anim_player.play("jump_up")
-		elif is_falling:
-				anim_player.play("jump_down")
+	if is_jumping and velocity.y < 0:
+		anim_player.play("jump_up")
+	elif is_falling:
+		anim_player.play("jump_down")
 
-		if velocity.y >= JUMP_APEX_THRESHOLD:
-				is_falling = true
-				is_jumping = false
+	if velocity.y >= JUMP_APEX_THRESHOLD:
+		is_falling = true
+		is_jumping = false
 
-		if is_on_floor():
-				is_jumping = false
-				is_falling = false
-				current_state = Player_state.MOVE
+	if is_on_floor():
+		is_jumping = false
+		is_falling = false
+		current_state = Player_state.MOVE
 
-		move_and_slide()
+	move_and_slide()
 
 func attack_state():
-		velocity = Vector2.ZERO
-		attack_area.visible = true
-		anim_player.play("attack")
-		can_attack = false
+	velocity = Vector2.ZERO
+	attack_area.visible = true
+	anim_player.play("attack")
+	can_attack = false
 
 func _on_animation_player_animation_finished(anim_name):
-		if anim_name == "attack":
-				attack_area.visible = false
-				current_state = Player_state.MOVE
-				can_attack = true
-				
-func player():
-	pass
+	if anim_name == "attack":
+		attack_area.visible = false
+		current_state = Player_state.MOVE
+		can_attack = true
+
+func _process(delta):
+	Global.player_position = global_position
 
 func update_attack_area_position():
-		var mouse_position = get_global_mouse_position()
-		var player_position = global_position
-		if mouse_position.x > player_position.x:
-				attack_shape_left.disabled = true
-				attack_shape_right.disabled = false
-		else:
-				attack_shape_left.disabled = false
-				attack_shape_right.disabled = true
+	var mouse_position = get_global_mouse_position()
+	var player_position = global_position
+	if mouse_position.x > player_position.x:
+		attack_shape_left.disabled = true
+		attack_shape_right.disabled = false
+	else:
+		attack_shape_left.disabled = false
+		attack_shape_right.disabled = true
+
--- enemy/Skeleton/enemy_1.gd
+++ enemy/Skeleton/enemy_1.gd
@@ -1,44 +1,59 @@
 extends CharacterBody2D
 
-# Массив точек патрулирования (заполните в инспекторе)
 @export var waypoints: Array[Vector2] = []
-# Скорость движения
-@export var speed: float = 85.0
-# Время ожидания в каждой точке
+@export var speed: float = 20.0
 @export var wait_time: float = 4.0
-# Сила гравитации
-@export var gravity: float = 980.0
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var anim = $AnimationPlayer
+@onready var attack_area = $attackarea/hert_box
+@onready var health_label = $Label
 
-# Ссылка на AnimatedSprite2D (добавьте в инспекторе)
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-
-# Индекс текущей точки патрулирования
 var current_waypoint_index: int = 0
-# Таймер для паузы в точках патрулирования
 var wait_timer: float = 0.0
-
-# Перечисление состояний
-enum State { PATROL, WAIT, CHASE }
-# Текущее состояние
+enum State { PATROL, WAIT, CHASE, ATTACK , DEATH }
 var current_state: State = State.PATROL
-
-# Ссылка на игрока
-var player: Node = null
+@export var attack_distance: float = 7.5
+var push_back_distance: float = 0.2
+@export var max_health = 50
+var health = max_health
+@export var damage = 10
 
 func _ready() -> void:
 	# Преобразуем локальные координаты точек патрулирования в глобальные
 	for i in range(waypoints.size()):
 		waypoints[i] = to_global(waypoints[i])
+	
+	$attackarea.connect("body_entered", Callable(self, "_on_attack_area_entered"))
+	$AnimationPlayer.connect("animation_finished", Callable(self, "_on_animation_finished"))
+	_update_health_label()  # Обновляем метку при запуске
+
+func _on_attack_area_entered(body):
+	if body.has_method("take_damage") and current_state == State.ATTACK:
+		body.take_damage(damage)
+		print("Enemy dealt damage: ", damage)
+
+func take_damage(amount: int) -> void:
+	health -= amount
+	print("Enemy took damage: ", amount, " Health remaining: ", health)
+	_update_health_label()  # Обновляем метку при изменении здоровья
+	if health <= 0:
+		current_state = State.DEATH
+
+func _update_health_label():
+	health_label.text = str(health)  # Обновляем текст метки
+
+func die() -> void:
+	print("Enemy died")
+	anim.play("daeth")
+	await anim.animation_finished
+	queue_free()
 
 func _physics_process(delta: float) -> void:
-	# Проверка на пустой массив waypoints
 	if waypoints.is_empty():
 		print("Ошибка: Массив waypoints пуст. Добавьте точки патрулирования в инспекторе.")
 		return
-
-	# Применяем гравитацию, если не на земле
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	velocity.y += gravity * delta
 
 	match current_state:
 		State.PATROL:
@@ -46,78 +61,85 @@ func _physics_process(delta: float) -> void:
 		State.WAIT:
 			_wait(delta)
 		State.CHASE:
-			# Преследуем игрока, только если на земле
-			if is_on_floor():
-				_chase(delta)
-			else:
-				current_state = State.PATROL  # Возвращаемся к патрулированию
-				player = null  # Сбрасываем игрока
-				print("Потерял игрока из виду, продолжаю патрулирование")
+			_chase(delta)
+		State.ATTACK:
+			_attack(delta)
+		State.DEATH:
+			die()
 
 func _patrol(delta: float) -> void:
-	# Получаем текущую точку патрулирования
 	var current_waypoint: Vector2 = waypoints[current_waypoint_index]
-
-	# Вычисляем направление и расстояние до текущей точки патрулирования
 	var direction_to_waypoint: Vector2 = current_waypoint - global_position
 	var distance_to_waypoint: float = direction_to_waypoint.length()
 
-	# Если достигли текущей точки патрулирования
 	if distance_to_waypoint < 5.0:
 		current_state = State.WAIT
 		wait_timer = 0.0
-		animated_sprite.play("idle") 
+		anim.play("idle")
 		print("Достигнута точка патрулирования. Ожидание...")
 	else:
-		# Двигаемся к текущей точке патрулирования
 		velocity = direction_to_waypoint.normalized() * speed
 		move_and_slide()
-		animated_sprite.play("run") 
-
-		# Разворачиваем спрайт врага в сторону движения
+		anim.play("run")
 		if velocity.x > 0:
-			animated_sprite.flip_h = false
+			sprite.flip_h = false
 		else:
-			animated_sprite.flip_h = true
+			sprite.flip_h = true
 
 func _wait(delta: float) -> void:
 	wait_timer += delta
 	if wait_timer >= wait_time:
 		current_state = State.PATROL
 		current_waypoint_index = (current_waypoint_index + 1) % waypoints.size()
-		print("Ожидание завершено. Продолжаю патрулирование")
 
 func _chase(delta: float) -> void:
-	if player != null:
-		var direction_to_player: Vector2 = player.global_position - global_position
-		var distance_to_player: float = direction_to_player.length()
+	var player_position = Global.player_position
+	var direction_to_player_x: float = player_position.x - global_position.x  # Получаем только разницу по X
+	var distance_to_player_x: float = abs(direction_to_player_x)  # Используем абсолютное значение для корректного направления
 
-		if distance_to_player > 200:  # Если игрок вышел из зоны обнаружения
-			current_state = State.PATROL  # Возвращаемся к патрулированию
-			player = null  # Обнуляем ссылку на игрока
-			print("Игрок потерян, возвращаюсь к патрулированию")
-		else:
-			velocity = direction_to_player.normalized() * speed
-			move_and_slide()
-
-			# Поворот спрайта в сторону игрока
-			if velocity.x > 0:
-				animated_sprite.flip_h = false
-			else:
-				animated_sprite.flip_h = true
-
-			print("Преследую игрока на расстоянии:", distance_to_player)
-	else:
+	if distance_to_player_x > 200:
 		current_state = State.PATROL
-		print("Игрок не найден, продолжаю патрулирование")
+		print("Игрок потерян, возвращаюсь к патрулированию")
+	elif distance_to_player_x <= attack_distance:
+		current_state = State.ATTACK
+		print("Игрок в зоне атаки. Начинаю атаку!")
+	else:
+		velocity.x = sign(direction_to_player_x) * speed  # Устанавливаем горизонтальную скорость в направлении игрока
+		velocity.y = 0  # Обнуляем вертикальную скорость
+		move_and_slide()
+		if velocity.x > 0:
+			sprite.flip_h = false
+		else:
+			sprite.flip_h = true
+			anim.play("run")
+
+func _attack(delta: float) -> void:
+	var player_position = Global.player_position
+	var direction_to_player_x: float = player_position.x - global_position.x
+	var distance_to_player_x: float = abs(direction_to_player_x)
+
+	if distance_to_player_x > 15:
+		# Если игрок вышел из зоны атаки, переходим к преследованию
+		current_state = State.CHASE
+		print("Игрок вышел из зоны атаки. Переход к преследованию.")
+	else:
+		anim.play("attack")
+		velocity = Vector2.ZERO  # Останавливаем движение во время атаки
+
+		# Отодвигаем врага в сторону от игрока
+		var push_back_vector_x: float = -sign(direction_to_player_x) * push_back_distance
+		global_position.x += push_back_vector_x * delta
+
+func _on_animation_finished(anim_name: String) -> void:
+	if anim_name == "attack" and current_state == State.ATTACK:
+		# Анимация атаки закончилась, возвращаемся к преследованию
+		current_state = State.CHASE
 
 func _on_detekted_zona_body_entered(body):
-	if body.has_method("player"):
-		player = body 
-		current_state = State.CHASE 
-		print("Игрок обнаружен! Перехожу в режим преследования")
+	if body.name == "Player":
+		current_state = State.CHASE
 
 func _on_detekted_zona_body_exited(body):
-	if body.has_method('player'):
+	if body.name == "Player":
 		current_state = State.PATROL
 
